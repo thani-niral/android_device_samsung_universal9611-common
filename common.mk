a46404ce22feda23e0f07b8095632f5a899a0556
thani-niral <gokul1103r@gmail.com>
2026-06-02 09:51:55 +0530

    common: Adjust preferred AAPT configuration for Galaxy A50
    - Update PRODUCT_AAPT_PREF_CONFIG to 440dpi to match the official display density of Samsung Galaxy A50 (exynos9610).

--- common.mk
+++ common.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 420dpi
+PRODUCT_AAPT_PREF_CONFIG := 440dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Health
